b451af18b6ed3f5e2e27585e846692330d1077c9
kevinlj2 <kevlok4@gmail.com>
2025-01-26 12:05:44 +0100

    arbeidskrav 1

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-resources.map((resource) => {
-    // Generer en liste med lenker fra sources
-    const sourcesHtml = resource.sources.map(source => 
-        `<li><a href="${source.url}" target="_blank">${source.title}</a></li>`
-    ).join(""); // Kombiner listen til én HTML-streng
+// Lager en array for alle knappene som skller hvilken tekst som skal vises
+const categories = ["HTML", "CSS", "JavaScript", "React", "Sanity and headless CMS"];
 
-    // Legg til denne ressursen i HTML-strengen
-    html += `
-        <section id="infoBox">
-            <h2>${resource.category}</h2>
-            <p>${resource.text}</p>
-            <ul>${sourcesHtml}</ul>
-        </section>
+// .map for å kunne peke på spesefikt object
+categories.map((category) => {
+  document.getElementById(category).addEventListener("click", () => {
+    // Ved å bruke .filter() kan man filtrere etter gitt parametere
+    // I linjen under sier koden at hvis kategorien samsvarer 
+    // med knappen som ble trykt på, så skriver den ut samsvarende tekst
+    const resource = resources.filter((res) => res.category === category)[0];
+
+    // Skriver inn i HTML
+    // Lager en template litterate for å kunne legge inn informasjonen
+    // .join("") blir brukt å fjerne komma som .map genererer mellom tekststrengene
+    document.getElementById("textBox").innerHTML = `    
+      <article>
+        <h3>${resource.category}</h3>
+        <p>${resource.text}</p>
+        <ul>
+          ${resource.sources.map((source) => `<li><a href="${source.url}">${source.title}</a></li>`).join("")}
+        </ul>
+      </article>
     `;
+  });
 });
-
-// Fyll inn HTML med teksten
-document.getElementById("content").innerHTML = html;
