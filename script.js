5cb487cd0fd0c51923db5bfdc900d5983745952b
kevinlj2 <kevlok4@gmail.com>
2025-01-30 07:23:38 +0100

    Oppdatert script, og css
    Siden lagget av å ha eventlistener i funksjonen
    
    også lagt inn en active class for å kunne si hvilken tab som er aktiv på siden

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Lager en array for alle knappene som skiller hvilken tekst som skal vises
 const categories = ["HTML", "CSS", "JavaScript", "React", "Sanity and headless CMS"];
+let activeButton = "HTML"
 
-// Deklarerer en funksjon for å vise informasjon basert på kategori
-function displayCategories(category) {
-  // Bruker .filter() for å filtrere gjennom resources for at kategoriene skal være 
-  // lik som den som har blitt trykket på
-  const resource = resources.filter((res) => res.category === category)[0];
+
 
 // Legger til event listeners for knappene
 // .map for å kunne peke på spesifikk kategori
@@ -17,6 +14,12 @@ categories.map((category) => {
   });
 });
 
+
+// Deklarerer en funksjon for å vise informasjon basert på kategori
+function displayCategories(category) {
+  // Bruker .filter() for å filtrere gjennom resources for at kategoriene skal være 
+  // lik som den som har blitt trykket på
+  const resource = resources.filter((res) => res.category === category)[0];
   // Skriver inn i HTML via document.getElementById("textBox").innerHTML
   // Lager en template litterate for å kunne legge inn informasjonen
   document.getElementById("textBox").innerHTML = `    
@@ -30,6 +33,11 @@ categories.map((category) => {
   `;
     // bruker .map for å kunne skille mellom URL-en og tittelen til kildene.
     // .join("") blir brukt å fjerne komma som .map genererer mellom tekststrengene
+
+    // Ved trykk på en knapp for matchende ID class-en "active" og fjerner den som har classen "active" samtidi
+    document.getElementById(activeButton).classList.remove("active");
+    document.getElementById(category).classList.add("active");
+    activeButton = category;
 }
 
 // Kaller på displayCateories og peker spesifikkt på HTML 
