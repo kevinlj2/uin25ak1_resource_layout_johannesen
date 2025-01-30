566167ce3972302b555423335febf503a2c080f6
kevinlj2 <kevlok4@gmail.com>
2025-01-30 12:36:10 +0100

    Arbeidskrav 1
    Fullført JavaScript.
    
    Må teste om CSS fungerer på mindre display - funker perfekt på 1920x1080

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 const categories = ["HTML", "CSS", "JavaScript", "React", "Sanity and headless CMS"];
 let activeButton = "HTML"
 
-
+// Kilde brukt for å lage funksjonen https://www.w3schools.com/jsref/jsref_substring.asp
+// Deklarerer en funksjon som sier at button-IDen "sanity" er det samme som "Sanity and headless CMS"
+function getCategoryId(category) {
+  return category === "Sanity and headless CMS" ? category.substring(0, 6) : category;
+}
 
 // Legger til event listeners for knappene
 // .map for å kunne peke på spesifikk kategori
 categories.map((category) => {
-  document.getElementById(category).addEventListener("click", () => {
+  document.getElementById(getCategoryId(category)).addEventListener("click", () => {
     // Funksjonen viser hvilken kategori som skal 
     // vises basert på hvilken som er trykket på
     displayCategories(category);
   });
 });
-
 
 // Deklarerer en funksjon for å vise informasjon basert på kategori
 function displayCategories(category) {
@@ -34,9 +37,9 @@ function displayCategories(category) {
     // bruker .map for å kunne skille mellom URL-en og tittelen til kildene.
     // .join("") blir brukt å fjerne komma som .map genererer mellom tekststrengene
 
-    // Ved trykk på en knapp for matchende ID class-en "active" og fjerner den som har classen "active" samtidi
-    document.getElementById(activeButton).classList.remove("active");
-    document.getElementById(category).classList.add("active");
+    // Ved trykk på en knapp for matchende ID class-en "active" og fjerner den som har classen "active" samtidig
+    document.getElementById(getCategoryId(activeButton)).classList.remove("active");
+    document.getElementById(getCategoryId(category)).classList.add("active");
     activeButton = category;
 }
 
